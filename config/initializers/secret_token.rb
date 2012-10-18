c5cd37a813f4b3a4be090f59a5e23ad7f9489b2b
jasperdiegostory <jasper@viewsy.com>
2012-10-18 16:39:13 +0100

    Inital commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '0dbe55e165ea3f29de2b59d4c5646d6de1211d5cdbc4cd35f380f6b82a825d5f809176c94aac7d76f4cd0dc1540f3143e6b611d83da03d6d549c7fa70b8ce127'
+FirstApp::Application.config.secret_token = 'f562edd552b419ab6cc7f46c3970ebfb5ab1539ff29d24b1b5e0c3fac8935eeedc250d626c148606181926efd1628f08817715adde8dc972a90c9bf4111c02ea'
